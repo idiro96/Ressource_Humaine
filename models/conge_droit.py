784d04fcf241f703640fd6d7989a5a5ee8dea9b8
benzinebasmaa <BENYOUCEF.Rabah@cetic.dz>
2023-12-19 16:12:53 +0100

    pull

--- models/conge_droit.py
+++ models/conge_droit.py
@@ -3,6 +3,7 @@
 from odoo import models, fields, api, _
 from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
+import calendar
 
 
 class RHCongeDroit(models.Model):
@@ -35,7 +36,10 @@ class RHCongeDroit(models.Model):
             jour = datetime.strptime(entrer_date1, '%Y-%m-%d').day
             mois = datetime.strptime(entrer_date1, '%Y-%m-%d').month
             year = datetime.strptime(entrer_date1, '%Y-%m-%d').year
-            debut_annee = date(dateDebut_object.year, mois, 1)
+
+
+
+            debut_annee = date(dateDebut_object.year, mois, jour)
             year_current = current_date.year
             current_date = dateDebut_object
             while current_date <= current_date2:
@@ -73,7 +77,11 @@ class RHCongeDroit(models.Model):
                         year_suivant = year +1
 
 
+                # anne_encours = str(year)  + '/' + str(year_suivant)
                 anne_encours = str(year)  + '/' + str(year_suivant)
+                print(empl.name)
+                print(current_date)
+                print(anne_encours)
                 conge_existe = self.env['rh.congedroit'].search(
                         [('exercice_conge', '=', anne_encours), ('id_personnel', '=', empl.id)])
                 if conge_existe:
@@ -91,14 +99,16 @@ class RHCongeDroit(models.Model):
                                     print('rabah')
                 else:
                     print(current_date)
-                    print(entrer_date2)
+                    print(debut_annee)
+
                     print(jour)
-                    if (jour > 15 and (current_date == entrer_date2)):
+                    if ((jour > 15) and (current_date == debut_annee)):
                         days_off = 0
                         print('iccci1')
                     else:
                         days_off = 2.5
                         print('iccci2')
+
                     conge_droit2 = self.env['rh.congedroit'].create({
                                         'id_personnel': empl.id,
                                         'exercice_conge': anne_encours,
@@ -113,102 +123,22 @@ class RHCongeDroit(models.Model):
                     })
                     print('rabah2')
 
+                res = calendar.monthrange(current_date.year, current_date.month)
+                day = res[1]
+                current_date = current_date + relativedelta(day=day)
+                current_date_mois = current_date.month
+                current_date_year = current_date.year
+                current_date = date(current_date_year, current_date_mois, 15)
                 current_date = relativedelta(months=1) + current_date
 
-
-                # res = calendar.monthrange(debut_fin.year, debut_fin.month)
-                # day = res[1]
-                # debut_fin_mois = debut_fin_mois + relativedelta(day=day)
-                # print(debut_annee)
-                # print(debut_fin)
-                # print(debut_fin_mois)
-                # months_passed = debut_fin_mois.day - entrer_date1.day
-                # print(months_passed)
-                #
-                # first_date = datetime.date(year, 7, 1)
-                # second_date = datetime.date(year_suivant, 6, 30)
-                #
-                # if current_date <= second_date:
-                #         anne_encours = str(year)  + '/' + str(year_suivant)
-                #         conge_existe= self.env['rh.congedroit'].search([('exercice_conge', '=', anne_encours),('id_personnel', '=', employee.id)])
-                #         if conge_existe:
-                #             print("exercice existe déja")
-                #             month_now = datetime.strptime(current_date, '%Y-%m-%d').month
-                #             conge_existe_month = self.env['rh.conge_droit_month'].search([('month', '=', month_now)])
-                #             if not conge_existe_month:
-                #                 days_off = days_off + 2.5
-                #                 conge_existe.write({'nbr_jour': days_off})
-                #         else:
-                #             if months_passed >= 15:
-                #                 days_off = days_off + 2.5
-                #                 conge_droit = self.env['rh.congedroit'].create({
-                #                     'id_personnel': employee.id,
-                #                     'exercice_conge': anne_encours,
-                #                     'nbr_jour': days_off,
-                #                     'nbr_jour_consomme': 0,
-                #                     'nbr_jour_reste': days_off,
-                #                 })
-                #                 conge_droit = self.env['rh.conge_droit_month'].create({
-                #                     'id_conge_droit': conge_droit.id,
-                #                     'month': mois,
-                #                 })
-                #             else :
-                #                 conge_droit = self.env['rh.congedroit'].create({
-                #                     'id_personnel': employee.id,
-                #                     'exercice_conge': anne_encours,
-                #                     'nbr_jour': 0,
-                #                     'nbr_jour_consomme': 0,
-                #                     'nbr_jour_reste': 0,
-                #                 })
-                #                 conge_droit = self.env['rh.conge_droit_month'].create({
-                #                     'id_conge_droit': conge_droit.id,
-                #                     'month': mois,
-                #                 })
-                #
-                #         formatted_date1 = relativedelta(months=1) + debut_annee
-                # else :
-                #
-                #     entrer_date1 = current_date
-                #     dateDebut_object = fields.Date.from_string(entrer_date1)
-                #     jour = datetime.strptime(entrer_date1, '%Y-%m-%d').day
-                #     mois = datetime.strptime(entrer_date1, '%Y-%m-%d').month
-                #     year = datetime.strptime(entrer_date1, '%Y-%m-%d').year
-                #     year_suivant = year + 1
-                #
-                #     anne_encours = str(year) + '/' + str(year_suivant)
-                #     conge_existe = self.env['rh.congedroit'].search([('exercice_conge', '=', anne_encours),('id_personnel', '=', employee.id)])
-                #     if conge_existe:
-                #         print("exercice existe déja")
-                #         month_now = datetime.strptime(current_date, '%Y-%m-%d').month
-                #         conge_existe_month = self.env['rh.conge_droit_month'].search([('month', '=', month_now)])
-                #         if not conge_existe_month:
-                #             days_off = days_off + 2.5
-                #             conge_existe.write({'nbr_jour': days_off})
-                #
-                #     else:
-                #             days_off = days_off + 2.5
-                #             conge_droit = self.env['rh.congedroit'].create({
-                #                 'id_personnel': employee.id,
-                #                 'exercice_conge': anne_encours,
-                #                 'nbr_jour': days_off,
-                #                 'nbr_jour_consomme': 0,
-                #                 'nbr_jour_reste': days_off,
-                #             })
-                #             conge_droit = self.env['rh.conge_droit_month'].create({
-                #                 'id_conge_droit': conge_droit.id,
-                #                 'month': mois,
-                #             })
-                #
-                #
-                #  # today_date = fields.Date.from_string(fields.Date.today())
-                #  # months_passed = (today_date.year - entrer_date.year) * 12 + today_date.month - entrer_date.month
-                #  # days_off = months_passed * 2.5
-                #  # employee.days_off = days_off
-                #
-                #
-
-
-
+                print(current_date)
+                print('date de mois')
+            conge_empl_total = self.env['rh.congedroit'].search(
+                [('id_personnel', '=', empl.id)])
+            jour_reste = 0
+            for cong in conge_empl_total:
+                jour_reste = cong.nbr_jour_reste + jour_reste
+            empl.write({'days_off': jour_reste})
 
 
 
