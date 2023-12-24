0c38ded1acbca016db0d7eb4b69e31df0cef1fcd
benzinebasmaa <BENYOUCEF.Rabah@cetic.dz>
2023-12-24 14:55:13 +0100

    pull

--- models/grade.py
+++ models/grade.py
@@ -11,7 +11,8 @@ class RHGrade(models.Model):
     code = fields.Char(String='Code', readonly=True, default=lambda self: _('New'))
     intitule_grade = fields.Char()
     corps_id = fields.Many2one(comodel_name='rh.corps')
-
+    filiere_id = fields.Many2one(comodel_name='rh.filiere')
+    loi_id = fields.Many2one(comodel_name='rh.loi')
 
 
     @api.model
@@ -21,3 +22,42 @@ class RHGrade(models.Model):
         result = super(RHGrade, self).create(vals)
         return result
 
+    @api.onchange('loi_id')
+    def _onchange_related_field_loi(self):
+        # This method will be called when the value of 'related_field' changes
+        # Update the domain for 'field1' based on the value of 'related_field'
+        print('teste')
+        for rec in self:
+            domain = []
+            if rec.loi_id:
+                print('teste')
+                filiere = self.env['rh.filiere'].search([('loi_id', '=', rec.loi_id.id)])
+                print(filiere)
+                domain.append(('id', 'in', filiere.ids))
+            else:
+                domain = ''
+
+        res = {'domain': {'filiere_id': domain}}
+        print(res)
+        return res
+
+    @api.onchange('filiere_id')
+    def _onchange_related_field_filier(self):
+        # This method will be called when the value of 'related_field' changes
+        # Update the domain for 'field1' based on the value of 'related_field'
+        print('teste')
+        for rec in self:
+            domain = []
+            if rec.filiere_id:
+                print('teste')
+                corps = self.env['rh.corps'].search([('filiere_id', '=', rec.filiere_id.id)])
+                print(corps)
+                if not rec.filiere_id:
+                    corps = self.env['rh.corps'].search([('loi_id', '=', rec.loi_id.id)])
+                    domain.append(('id', 'in', corps.ids))
+            else:
+                domain = ''
+
+        res = {'domain': {'corps_id': domain}}
+        print(res)
+        return res
