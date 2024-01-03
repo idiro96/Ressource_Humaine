dd2fb4ff4da12a9ade6a17016299f61a500cc5b0
lotfi-cetic <lotfi@cetic.dz>
2024-01-03 10:29:07 +0100

    Your commit message here

--- models/hr_holidays_inherit.py
+++ models/hr_holidays_inherit.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
 from odoo.addons.hr_holidays.models.hr_holidays import Holidays
-from odoo.addons_cetic_invist.gestion_temps.models.hr_employee_inherit import HrEmployeInherited
 from odoo.exceptions import ValidationError
 
 
