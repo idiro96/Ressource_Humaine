5ab112b600c8599c51dd06e62be2947902ea9350
lotfi-cetic <lotfi@cetic.dz>
2024-01-30 08:57:54 +0100

    30/01/2024

--- models/commission_line.py
+++ models/commission_line.py
@@ -6,7 +6,6 @@ from odoo import models, fields, api, _
 class RHcommissionLine(models.Model):
     _name = 'rh.commission.line'
 
-
     employee_id = fields.Many2one('hr.employee')
     sanction_id = fields.Many2one('rh.sanction')
     department_id = fields.Char()
