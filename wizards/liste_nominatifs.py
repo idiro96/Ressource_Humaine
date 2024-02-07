38aac47ad4d4d9480241e8950f8e5763f7fef542
idiro96 <idir.zaboot@cetic.dz>
2024-02-07 14:45:28 +0100

    up

--- wizards/liste_nominatifs.py
+++ wizards/liste_nominatifs.py
@@ -11,20 +11,20 @@ class ListeNominatifs(models.TransientModel):
     def print_report(self):
         return self.env.ref('ressource_humaine.action_liste_nominatife').report_action(self)
 
-#
-# class PlanningCongeReport(models.AbstractModel):
-#     _name = 'report.ressource_humaine.planning_conge'
-#
-#     @api.model
-#     def get_report_values(self, docids, data=None):
-#         conge = self.env['planning.conge'].browse(docids[0])
-#
-#         employees = self.env['hr.employee'].search([('department_id', '=', conge.department_id.id)])
-#
-#         report_data = {
-#             'conge': conge,
-#             'company': self.env.user.company_id,
-#             'employees': employees,
-#         }
-#
-#         return report_data
+
+class PlanningCongeReport(models.AbstractModel):
+    _name = 'report.ressource_humaine.liste_nominatife_employee'
+
+    @api.model
+    def get_report_values(self, docids, data=None):
+        # conge = self.env['planning.conge'].browse(docids[0])
+
+        employees = self.env['hr.employee'].browse(docids[0])
+
+        report_data = {
+            # 'conge': conge,
+            'company': self.env.user.company_id,
+            'employees': employees,
+        }
+
+        return report_data
