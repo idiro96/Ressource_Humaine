741bd6888ffbd77ae66dc21846112178b00ee531
benzinebasmaa <BENYOUCEF.Rabah@cetic.dz>
2024-02-08 11:55:58 +0100

    push

--- models/fiche_evaluation.py
+++ models/fiche_evaluation.py
@@ -11,11 +11,29 @@ class RHFicheEvaluation(models.Model):
 
     date_evaluation = fields.Date()
     employee_id = fields.Many2one('hr.employee')
-    grade_id = fields.Many2one('rh.grade')
-    job_id = fields.Many2one('hr.job')
-    echelon_id = fields.Many2one('rh.echelon')
+    grade_id = fields.Many2one('rh.grade',compute='_onchange_employee_id', store=True)
+    job_id = fields.Many2one('hr.job',compute='_onchange_employee_id', store=True)
+    echelon_id = fields.Many2one('rh.echelon',compute='_onchange_employee_id', store=True)
     date_grade = fields.Date()
     note = fields.Integer()
     observation = fields.Char()
+    fiche_evaluation_file = fields.Binary()
+
+    # @api.model
+    # def create(self, vals):
+    #     evalutation = super(RHFicheEvaluation, self).create(vals)
+    #     for rec in self:
+    #         vals['grade_id'] = evalutation.employee_id.grade_id
+    #         vals['job_id'] = evalutation.employee_id.job_id
+    #         vals['echelon_id'] = evalutation.employee_id.echelon_id
+    #     return evalutation
+
+    @api.depends('employee_id')
+    def _onchange_employee_id(self):
+        for rec in self:
+            rec.grade_id = rec.employee_id.grade_id
+            rec.job_id = rec.employee_id.job_id
+            rec.echelon_id = rec.employee_id.echelon_id
+
 
 
