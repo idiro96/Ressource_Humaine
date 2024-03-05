5a63c83a51dc0f8e8c58106028d9b25e4b4431fe
lotfi-cetic <lotfi@cetic.dz>
2024-03-05 10:37:07 +0100

    05/03/2024

--- models/hr_job_inherit.py
+++ models/hr_job_inherit.py
@@ -19,7 +19,7 @@ class HrJobInherited(models.Model):
     poste_organique = fields.Selection(selection=[('organique', 'منصب هيكلي'), ('squelaire', 'منصب عضوي')],
                                        readonly=False)
     max_employee = fields.Integer(default=1, store=True)
-    nombre_de_postes_vacants = fields.Integer(compute='_compute_nombre_de_postes_vacants', store=True,)
+    nombre_de_postes_vacants = fields.Integer(compute='_compute_nombre_de_postes_vacants', store=True, )
     code_type_fonction = fields.Char(related='nature_travail_id.code_type_fonction',
                                      string='Code Type Fonction', store=True)
 
@@ -37,5 +37,14 @@ class HrJobInherited(models.Model):
     @api.multi
     def unlink(self):
         raise UserError(
-                    "Vous ne pouvez pas supprimer cet enregistrement")
+            "Vous ne pouvez pas supprimer cet enregistrement")
         return super(HrJobInherited, self).unlink()
+
+    @api.depends('no_of_recruitment', 'employee_ids.job_id', 'employee_ids.active', 'employee_ids.methode_embauche')
+    def _compute_employees(self):
+        for job in self:
+            if job.employee_ids.methode_embauche == 'recrutement':
+                employee_data = self.env['hr.employee'].read_group([('job_id', 'in', job.ids)], ['job_id'], ['job_id'])
+                result = dict((data['job_id'][0], data['job_id_count']) for data in employee_data)
+                job.no_of_employee = result.get(job.id, 0)
+                job.expected_employees = result.get(job.id, 0) + job.no_of_recruitment
