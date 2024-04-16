9a18caaabb05722a5da08e5bd17f6165bdc2b171
lotfi-cetic <lotfi@cetic.dz>
2024-04-16 15:58:44 +0100

    16/04/2024 - 15:58

--- models/planning_surveillance_line.py
+++ models/planning_surveillance_line.py
@@ -11,10 +11,10 @@ class RHPlanningLine(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _mail_post_access = 'read'
 
-    employee_id = fields.Many2many('hr.employee', track_visibility='onchange')
-    emphy_id = fields.Many2one('rh.emphy', track_visibility='onchange')
+    employee_id = fields.Many2many('hr.employee', tracking=True)
+    emphy_id = fields.Many2one('rh.emphy', tracking=True)
     planning_survellance_id = fields.Many2one('rh.planning')
-    president_emphy = fields.Many2one('hr.employee', track_visibility='onchange')
+    president_emphy = fields.Many2one('hr.employee', tracking=True)
     date_examen = fields.Date(related="planning_survellance_id.date_surveillance", store=True)
     time_start = fields.Char(related="planning_survellance_id.time_surveillance_start")
     time_end = fields.Char(related="planning_survellance_id.time_surveillance_end")
@@ -58,3 +58,73 @@ class RHPlanningLine(models.Model):
             #             break
 
         return {'domain': {'employee_id': [('id', 'not in', domain)]}}
+
+    @api.model
+    def create(self, vals):
+        new_record = super(RHPlanningLine, self).create(vals)
+
+        message_body = "تحديث قائمة المراقبون:<br/>"
+        # if 'employee_id' in vals:
+        #     employee_ids = [int(id) for id in vals.get('employee_id') if isinstance(id, int)]
+        #     employee_names = self.env['hr.employee'].search([('id', 'in', employee_ids)]).mapped('name')
+        #     for employee_name in employee_names:
+        #         message_body += f"  • تعيين الموظف: {employee_name}<br/>"
+        if 'emphy_id' in vals:
+            intitule_emphy = self.env['rh.emphy'].browse(vals['emphy_id']).intitule_emphy
+            message_body += f"  • المدرج: {intitule_emphy}<br/>"
+        if 'president_emphy' in vals:
+            president_name = self.env['hr.employee'].browse(vals['president_emphy']).name
+            message_body += f"  • رئيس المدرج: {president_name}<br/>"
+
+        if message_body:
+            new_record.planning_survellance_id.message_post(body=message_body)
+
+        return new_record
+
+    @api.multi
+    def unlink(self):
+        for record in self:
+            message_body = f"قد تم حذف مراقبة المدرج {record.emphy_id.intitule_emphy}"
+            if message_body:
+                record.planning_survellance_id.message_post(body=message_body)
+
+        return super(RHPlanningLine, self).unlink()
+
+    def write(self, vals):
+        # original_employee_id = self.employee_id
+        original_emphy_id = self.emphy_id
+        original_president_emphy = self.president_emphy
+
+        result = super(RHPlanningLine, self).write(vals)
+
+        if any(field in vals for field in
+               ['emphy_id', 'president_emphy']):
+            self._track_changes(original_emphy_id, original_president_emphy)
+
+        return result
+
+    def _track_changes(self, original_emphy_id, original_president_emphy):
+        if self.planning_survellance_id:
+            new_intitule_emphy = self.emphy_id.intitule_emphy if self.emphy_id else False
+            old_intitule_emphy = original_emphy_id.intitule_emphy if original_emphy_id else False
+            new_president_emphy_name = self.president_emphy.name if self.president_emphy else False
+            old_president_emphy_name = original_president_emphy.name if original_president_emphy else False
+
+            message_body = 'تحديث قائمة المراقبون:<br/>'
+            if old_intitule_emphy != new_intitule_emphy:
+                if old_intitule_emphy and new_intitule_emphy:
+                    message_body += f"  • تغيير المدرج من {old_intitule_emphy} إلى {new_intitule_emphy}<br/>"
+                elif old_intitule_emphy:
+                    message_body += f"  • حذف المدرج: {old_intitule_emphy}<br/>"
+                elif new_intitule_emphy:
+                    message_body += f"  • تعيين المدرج: {new_intitule_emphy}<br/>"
+
+            if old_president_emphy_name != new_president_emphy_name:
+                if old_president_emphy_name and new_president_emphy_name:
+                    message_body += f"  • تغيير رئيس المدرج {old_president_emphy_name} إلى {new_president_emphy_name}<br/>"
+                elif old_president_emphy_name:
+                    message_body += f"  • حذف رئيس المدرج: {old_president_emphy_name}<br/>"
+                elif new_president_emphy_name:
+                    message_body += f"  • تعيين رئيس المدرج: {new_president_emphy_name}<br/>"
+
+            self.planning_survellance_id.message_post(body=message_body)
--- models/promotion_ligne.py
+++ models/promotion_ligne.py
@@ -21,15 +21,15 @@ class RHPromotionLine(models.Model):
     date_new_grade = fields.Date()
     date_grade = fields.Date()
     type_fonction_id = fields.Many2one('rh.type.fonction')
-    promotion_line_file_line = fields.Binary(track_visibility='onchange')
+    promotion_line_file_line = fields.Binary()
     duree = fields.Integer()
     imprimer = fields.Boolean(Default=False)
-    code_line = fields.Char(track_visibility='onchange')
+    code_line = fields.Char(tracking=True)
     date_creation = fields.Char(compute="_compute_date", store=True)
     ref_promotion = fields.Char()
     date_ref_promotion = fields.Date()
     date_signature = fields.Date(related='promotion_id.date_signature')
-    ancien_index = fields.Integer(track_visibility='onchange')
+    ancien_index = fields.Integer(tracking=True)
     categorie_id = fields.Many2one('rh.categorie')
     section_id = fields.Many2one('rh.section')
     echelon_id = fields.Many2one('rh.echelon')
@@ -48,5 +48,35 @@ class RHPromotionLine(models.Model):
                 date_creation = datetime_object[0]
                 record.date_creation = date_creation
 
+    @api.multi
+    def unlink(self):
+        for record in self:
+            message_body = f"قد تم حذف الموظف {record.employee_id.name} من قائمة الموظفين"
+            if message_body:
+                record.promotion_id.message_post(body=message_body)
 
+        return super(RHPromotionLine, self).unlink()
+
+    def write(self, vals):
+        original_code_line = self.code_line
+        original_ancien_index = self.ancien_index
+
+        result = super(RHPromotionLine, self).write(vals)
+
+        if any(field in vals for field in
+               ['code_line', 'ancien_index']):
+            self._track_changes(original_code_line, original_ancien_index)
+
+        return result
+
+    def _track_changes(self, original_code_line, original_ancien_index):
+        if self.promotion_id:
+            message_body = 'تحديث قائمة الموظفين:<br/>'
+            if self.code_line != original_code_line:
+                message_body += f"  • تغيير رقم مقرر {self.employee_id.name} من {original_code_line} إلى {self.code_line}<br/>"
+
+            if self.ancien_index != original_ancien_index:
+                message_body += f"  • تغيير الرقم الإستدلالي لطلبة {self.employee_id.name} من {original_ancien_index} إلى {self.ancien_index}<br/>"
+
+            self.promotion_id.message_post(body=message_body)
 
