850070eb31c52244bf9ddff485bead56e4028b48
lotfi-cetic <lotfi@cetic.dz>
2024-06-05 09:41:30 +0100

    lotfi-cetic : 05/06/2024 - 09:41

--- reports/arret_salaire_rapport.py
+++ reports/arret_salaire_rapport.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from odoo import models, api, _, fields
 
 
-
 class ArretSalaireReport(models.AbstractModel):
     _name = 'report.ressource_humaine.arret_salaire_rapport'
 
@@ -11,6 +10,29 @@ class ArretSalaireReport(models.AbstractModel):
     def get_report_values(self, docids, data=None):
 
         decision = self.env['arret.salaire'].browse(docids[0])
+        formatted_date_motif = None
+        formatted_date_avancement = None
+        formatted_date_arret_salaire = None
+        formatted_date_titularisation = None
+        formatted_date_decision_titularisation = None
+        if decision.date_motif:
+            rapport_date_motif = decision.date_motif
+            formatted_date_motif = datetime.strptime(rapport_date_motif, "%Y-%m-%d").strftime("%Y/%m/%d")
+        if decision.employee_id.date_avancement:
+            rapport_date_avancement = decision.employee_id.date_avancement
+            formatted_date_avancement = datetime.strptime(rapport_date_avancement, "%Y-%m-%d").strftime("%Y/%m/%d")
+        if decision.date_arret_salaire:
+            rapport_date_arret_salaire = decision.date_arret_salaire
+            formatted_date_arret_salaire = datetime.strptime(rapport_date_arret_salaire, "%Y-%m-%d").strftime(
+                "%Y/%m/%d")
+        if decision.date_titularisation:
+            rapport_date_titularisation = decision.date_titularisation
+            formatted_date_titularisation = datetime.strptime(rapport_date_titularisation,
+                                                              "%Y-%m-%d").strftime("%Y/%m/%d")
+        if decision.date_decision_titularisation:
+            rapport_date_decision_titularisation = decision.date_decision_titularisation
+            formatted_date_decision_titularisation = datetime.strptime(rapport_date_decision_titularisation,
+                                                                       "%Y-%m-%d").strftime("%Y/%m/%d")
 
         # nom_employee = self.env['rh.fin.relation'].browse(docids)
         # print(nom_employee)
@@ -25,6 +47,11 @@ class ArretSalaireReport(models.AbstractModel):
 
         report_data = {
             'decision': decision,
+            'date_motif': formatted_date_motif,
+            'date_avancement': formatted_date_avancement,
+            'date_arret_salaire': formatted_date_arret_salaire,
+            'date_titularisation': formatted_date_titularisation,
+            'date_decision_titularisation': formatted_date_decision_titularisation,
             # 'nom_employee': nom_employee,
             # 'employee': employee,
             # 'promotion': promotion,
