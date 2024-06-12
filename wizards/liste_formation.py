2d0080dc2de75ce72d0fc778b7e6e01f7e6bfc8f
lotfi-cetic <lotfi@cetic.dz>
2024-06-12 11:01:36 +0100

    lotfi-cetic : 12/06/2024 - 11:01

--- wizards/liste_formation.py
+++ wizards/liste_formation.py
@@ -21,6 +21,9 @@ class ListeFormation(models.TransientModel):
     def print_report(self):
         return self.env.ref('ressource_humaine.action_liste_formation_report').report_action(self)
 
+    def export_report(self):
+        return self.env.ref('ressource_humaine.action_liste_formation_xlsx').report_action(self)
+
 
 class ListeFormationReport(models.AbstractModel):
     _name = 'report.ressource_humaine.liste_formation_report'
@@ -66,3 +69,67 @@ class ListeFormationReport(models.AbstractModel):
         }
 
         return report_data
+
+
+class ListeFormationXLS(models.AbstractModel):
+    _name = 'report.ressource_humaine.liste_formation_xlsx'
+    _inherit = 'report.report_xlsx.abstract'
+
+    def generate_xlsx_report(self, workbook, data, objs):
+        formation_wizard = self._get_objs_for_report(objs.ids, data)
+        formation_lines = self.env['rh.formation.line'].search(
+            [('formation_id.date_debut_formation', '>=', formation_wizard.date_debut),
+             ('formation_id.date_fin_formation', '<=', formation_wizard.date_fin)])
+        report_date_debut = formation_wizard.date_debut
+        date_debut = datetime.strptime(report_date_debut, "%Y-%m-%d").strftime("%Y/%m/%d")
+        report_date_fin = formation_wizard.date_fin
+        date_fin = datetime.strptime(report_date_fin, "%Y-%m-%d").strftime("%Y/%m/%d")
+
+        format1 = workbook.add_format(
+            {'font_size': 12, 'align': 'center', 'valign': 'vcenter', 'border': 1, 'bg_color': '#D3D3D3', 'bold': True})
+        format1.set_text_wrap()
+        format1.set_align('center')
+        format1.set_valign('vcenter')
+        format2 = workbook.add_format(
+            {'font_size': 10, 'align': 'center', 'valign': 'vcenter', 'border': 1, 'bg_color': '#D3D3D3', 'bold': True})
+        format2.set_text_wrap()
+        format2.set_align('center')
+        format2.set_valign('vcenter')
+        format3 = workbook.add_format(
+            {'font_size': 10, 'align': 'center', 'valign': 'vcenter', 'border': 1, 'bg_color': '#FFFFFF'})
+        date_format = workbook.add_format({'font_size': 10, 'align': 'center', 'valign': 'vcenter', 'border': 1,
+                                           'bg_color': '#FFFFFF', 'num_format': 'J MMMM AAAA'})
+        date_format2 = workbook.add_format({'font_size': 10, 'align': 'center', 'valign': 'vcenter', 'border': 1,
+                                            'bg_color': '#FFFFFF', 'num_format': 'AAAA - MM - JJ'})
+        title_format = workbook.add_format(
+            {'bold': True, 'font_size': 32, 'align': 'center', 'valign': 'vcenter', 'border': 2, 'bg_color': '#FFFFFF'})
+        sheet = workbook.add_worksheet('قائمة الموظفين المستفيدين من التكوين')
+        sheet.right_to_left()
+
+        sheet.set_row(6, 35)
+        for row_num in range(7, len(formation_lines) + 1):
+            sheet.set_row(row_num, 30)
+
+        sheet.set_column(0, 0, 30)
+        sheet.set_column(1, 1, 40)
+        sheet.set_column(2, 2, 25)
+        sheet.set_column(3, 3, 30)
+        sheet.set_column(4, 4, 30)
+
+        sheet.write(6, 0, 'الإسم و اللقب', format1)
+        sheet.write(6, 1, 'الرتبة/الوظيفة', format1)
+        sheet.write(6, 2, 'موضوع الدورة التكوينية', format1)
+        sheet.write(6, 3, 'المدة و التاريخ', format1)
+        sheet.write(6, 4, 'ملاحــــــظة', format1)
+
+        sheet.merge_range('A2:F4', f"{date_fin} إلى {date_debut}قائمة الموظفين المستفيدين من التكوين من ",
+                          title_format)
+
+        row = 7
+        for index, line in enumerate(formation_lines, start=1):
+            sheet.write(row, 0, line.employee_id.name, format3)
+            sheet.write(row, 1, line.employee_id.grade_id.intitule_grade, format3)
+            sheet.write(row, 2, line.formation_id.intitule_formation, format3)
+            sheet.write(row, 3, f"{line.formation_id.date_fin_formation} / {line.formation_id.date_debut_formation}", format3)
+            sheet.write(row, 4, line.formation_id.organisme_id.rs_organisme or '/', format3)
+            row += 1
