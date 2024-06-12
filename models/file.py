cf0afa0c2a026c1184dadc687638cc7605eafc43
lotfi-cetic <lotfi@cetic.dz>
2024-06-12 13:47:34 +0100

    lotfi-cetic : 12/06/2024 - 13:47

--- models/file.py
+++ models/file.py
@@ -5,8 +5,8 @@ from odoo import models, fields, api, _
 
 class RHFile(models.Model):
     _name = 'rh.file'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-    _mail_post_access = 'read'
+    # _inherit = ['mail.thread', 'mail.activity.mixin']
+    # _mail_post_access = 'read'
 
     fichier = fields.Binary()
     description_fichier = fields.Char(tracking=True)
@@ -26,98 +26,98 @@ class RHFile(models.Model):
     type_file_id = fields.Many2one('rh.type.file', tracking=True)
     employee_id = fields.Many2one('hr.employee')
 
-    @api.model
-    def create(self, vals):
-        new_record = super(RHFile, self).create(vals)
-
-        # Post a message with the values of the tracked fields
-        message_body = "تحديث قائمة الملفات :<br/>"
-        if 'type_file_id' in vals:
-            type_file_id_intitule = self.env['rh.type.file'].browse(vals['type_file_id']).intitule
-            message_body += f"  • تعيين نوع الملف: {type_file_id_intitule}<br/>"
-        if 'description_fichier' in vals:
-            message_body += f"  • تعيين وصف الملف: {vals['description_fichier']}<br/>"
-
-        if message_body:
-            new_record.formation_id.message_post(body=message_body)
-            new_record.employee_id.message_post(body=message_body)
-            new_record.sanction_id.message_post(body=message_body)
-            new_record.organisme_id.message_post(body=message_body)
-            new_record.fin_relation_id.message_post(body=message_body)
-            new_record.cabinet_medicale_id.message_post(body=message_body)
-            new_record.enfant_id.message_post(body=message_body)
-            new_record.conjoint_id.message_post(body=message_body)
-            new_record.absence_id.message_post(body=message_body)
-            new_record.avancement_id.message_post(body=message_body)
-            new_record.promotion_id.message_post(body=message_body)
-            new_record.accident_travail_id.message_post(body=message_body)
-
-        return new_record
-
-    @api.multi
-    def unlink(self):
-        for record in self:
-            message_body = f"قد تم حذف الملف {record.description_fichier} من قائمة الملفات"
-            if message_body:
-                record.formation_id.message_post(body=message_body)
-                record.employee_id.message_post(body=message_body)
-                record.sanction_id.message_post(body=message_body)
-                record.organisme_id.message_post(body=message_body)
-                record.fin_relation_id.message_post(body=message_body)
-                record.cabinet_medicale_id.message_post(body=message_body)
-                record.enfant_id.message_post(body=message_body)
-                record.conjoint_id.message_post(body=message_body)
-                record.absence_id.message_post(body=message_body)
-                record.avancement_id.message_post(body=message_body)
-                record.promotion_id.message_post(body=message_body)
-                record.accident_travail_id.message_post(body=message_body)
-
-        return super(RHFile, self).unlink()
-
-    def write(self, vals):
-        # Store the original values of the fields before the write operation
-        original_type_file_id = self.type_file_id
-        original_description_fichier = self.description_fichier
-
-        # Call super to perform the default behavior of write
-        result = super(RHFile, self).write(vals)
-
-        # Check if any of the tracked fields have changed
-        if any(field in vals for field in ['type_file_id', 'description_fichier']):
-            self._track_changes(original_type_file_id, original_description_fichier)
-
-        return result
-
-    def _track_changes(self, original_type_file_id, original_description_fichier):
-        # Post a message in the chatter of the related formation record
-        if (self.formation_id or self.employee_id or self.sanction_id or self.organisme_id or self.fin_relation_id or
-                self.cabinet_medicale_id or self.enfant_id or self.conjoint_id or self.absence_id or self.avancement_id
-                or self.promotion_id or self.accident_travail_id):
-            new_type_file_id_intitule = self.type_file_id.intitule if self.type_file_id else False
-            old_type_file_id_intitule = original_type_file_id.intitule if original_type_file_id else False
-
-            message_body = 'تحديث قائمة الملفات :<br/>'
-            if old_type_file_id_intitule != new_type_file_id_intitule:
-                if old_type_file_id_intitule and new_type_file_id_intitule:
-                    message_body += f"  • تغيير نوع الملف {old_type_file_id_intitule} إلى {new_type_file_id_intitule}<br/>"
-                elif old_type_file_id_intitule:
-                    message_body += f"  • حذف نوع الملف: {old_type_file_id_intitule}<br/>"
-                elif new_type_file_id_intitule:
-                    message_body += f"  • تعيين نوع الملف: {new_type_file_id_intitule}<br/>"
-
-            if self.description_fichier != original_description_fichier:
-                message_body += f"  • تغيير وصف الملف {new_type_file_id_intitule} من {original_description_fichier or 'بدون وصف'} إلى {self.description_fichier or 'بدون وصف'}<br/>"
-
-            if message_body:
-                self.formation_id.message_post(body=message_body)
-                self.employee_id.message_post(body=message_body)
-                self.sanction_id.message_post(body=message_body)
-                self.organisme_id.message_post(body=message_body)
-                self.fin_relation_id.message_post(body=message_body)
-                self.cabinet_medicale_id.message_post(body=message_body)
-                self.enfant_id.message_post(body=message_body)
-                self.conjoint_id.message_post(body=message_body)
-                self.absence_id.message_post(body=message_body)
-                self.avancement_id.message_post(body=message_body)
-                self.promotion_id.message_post(body=message_body)
-                self.accident_travail_id.message_post(body=message_body)
+    # @api.model
+    # def create(self, vals):
+    #     new_record = super(RHFile, self).create(vals)
+    #
+    #     # Post a message with the values of the tracked fields
+    #     message_body = "تحديث قائمة الملفات :<br/>"
+    #     if 'type_file_id' in vals:
+    #         type_file_id_intitule = self.env['rh.type.file'].browse(vals['type_file_id']).intitule
+    #         message_body += f"  • تعيين نوع الملف: {type_file_id_intitule}<br/>"
+    #     if 'description_fichier' in vals:
+    #         message_body += f"  • تعيين وصف الملف: {vals['description_fichier']}<br/>"
+    #
+    #     if message_body:
+    #         new_record.formation_id.message_post(body=message_body)
+    #         new_record.employee_id.message_post(body=message_body)
+    #         new_record.sanction_id.message_post(body=message_body)
+    #         new_record.organisme_id.message_post(body=message_body)
+    #         new_record.fin_relation_id.message_post(body=message_body)
+    #         new_record.cabinet_medicale_id.message_post(body=message_body)
+    #         new_record.enfant_id.message_post(body=message_body)
+    #         new_record.conjoint_id.message_post(body=message_body)
+    #         new_record.absence_id.message_post(body=message_body)
+    #         new_record.avancement_id.message_post(body=message_body)
+    #         new_record.promotion_id.message_post(body=message_body)
+    #         new_record.accident_travail_id.message_post(body=message_body)
+    #
+    #     return new_record
+    #
+    # @api.multi
+    # def unlink(self):
+    #     for record in self:
+    #         message_body = f"قد تم حذف الملف {record.description_fichier} من قائمة الملفات"
+    #         if message_body:
+    #             record.formation_id.message_post(body=message_body)
+    #             record.employee_id.message_post(body=message_body)
+    #             record.sanction_id.message_post(body=message_body)
+    #             record.organisme_id.message_post(body=message_body)
+    #             record.fin_relation_id.message_post(body=message_body)
+    #             record.cabinet_medicale_id.message_post(body=message_body)
+    #             record.enfant_id.message_post(body=message_body)
+    #             record.conjoint_id.message_post(body=message_body)
+    #             record.absence_id.message_post(body=message_body)
+    #             record.avancement_id.message_post(body=message_body)
+    #             record.promotion_id.message_post(body=message_body)
+    #             record.accident_travail_id.message_post(body=message_body)
+    #
+    #     return super(RHFile, self).unlink()
+    #
+    # def write(self, vals):
+    #     # Store the original values of the fields before the write operation
+    #     original_type_file_id = self.type_file_id
+    #     original_description_fichier = self.description_fichier
+    #
+    #     # Call super to perform the default behavior of write
+    #     result = super(RHFile, self).write(vals)
+    #
+    #     # Check if any of the tracked fields have changed
+    #     if any(field in vals for field in ['type_file_id', 'description_fichier']):
+    #         self._track_changes(original_type_file_id, original_description_fichier)
+    #
+    #     return result
+    #
+    # def _track_changes(self, original_type_file_id, original_description_fichier):
+    #     # Post a message in the chatter of the related formation record
+    #     if (self.formation_id or self.employee_id or self.sanction_id or self.organisme_id or self.fin_relation_id or
+    #             self.cabinet_medicale_id or self.enfant_id or self.conjoint_id or self.absence_id or self.avancement_id
+    #             or self.promotion_id or self.accident_travail_id):
+    #         new_type_file_id_intitule = self.type_file_id.intitule if self.type_file_id else False
+    #         old_type_file_id_intitule = original_type_file_id.intitule if original_type_file_id else False
+    #
+    #         message_body = 'تحديث قائمة الملفات :<br/>'
+    #         if old_type_file_id_intitule != new_type_file_id_intitule:
+    #             if old_type_file_id_intitule and new_type_file_id_intitule:
+    #                 message_body += f"  • تغيير نوع الملف {old_type_file_id_intitule} إلى {new_type_file_id_intitule}<br/>"
+    #             elif old_type_file_id_intitule:
+    #                 message_body += f"  • حذف نوع الملف: {old_type_file_id_intitule}<br/>"
+    #             elif new_type_file_id_intitule:
+    #                 message_body += f"  • تعيين نوع الملف: {new_type_file_id_intitule}<br/>"
+    #
+    #         if self.description_fichier != original_description_fichier:
+    #             message_body += f"  • تغيير وصف الملف {new_type_file_id_intitule} من {original_description_fichier or 'بدون وصف'} إلى {self.description_fichier or 'بدون وصف'}<br/>"
+    #
+    #         if message_body:
+    #             self.formation_id.message_post(body=message_body)
+    #             self.employee_id.message_post(body=message_body)
+    #             self.sanction_id.message_post(body=message_body)
+    #             self.organisme_id.message_post(body=message_body)
+    #             self.fin_relation_id.message_post(body=message_body)
+    #             self.cabinet_medicale_id.message_post(body=message_body)
+    #             self.enfant_id.message_post(body=message_body)
+    #             self.conjoint_id.message_post(body=message_body)
+    #             self.absence_id.message_post(body=message_body)
+    #             self.avancement_id.message_post(body=message_body)
+    #             self.promotion_id.message_post(body=message_body)
+    #             self.accident_travail_id.message_post(body=message_body)
--- models/promotion_ligne.py
+++ models/promotion_ligne.py
@@ -5,8 +5,8 @@ from odoo import models, fields, api, _
 
 class RHPromotionLine(models.Model):
     _name = 'rh.promotion.line'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-    _mail_post_access = 'read'
+    # _inherit = ['mail.thread', 'mail.activity.mixin']
+    # _mail_post_access = 'read'
 
     date_examin_professionnel = fields.Date()
     date_promotion = fields.Date()
@@ -48,35 +48,35 @@ class RHPromotionLine(models.Model):
                 date_creation = datetime_object[0]
                 record.date_creation = date_creation
 
-    @api.multi
-    def unlink(self):
-        for record in self:
-            message_body = f"قد تم حذف الموظف {record.employee_id.name} من قائمة الموظفين"
-            if message_body:
-                record.promotion_id.message_post(body=message_body)
-
-        return super(RHPromotionLine, self).unlink()
-
-    def write(self, vals):
-        original_code_line = self.code_line
-        original_ancien_index = self.ancien_index
-
-        result = super(RHPromotionLine, self).write(vals)
-
-        if any(field in vals for field in
-               ['code_line', 'ancien_index']):
-            self._track_changes(original_code_line, original_ancien_index)
-
-        return result
-
-    def _track_changes(self, original_code_line, original_ancien_index):
-        if self.promotion_id:
-            message_body = 'تحديث قائمة الموظفين:<br/>'
-            if self.code_line != original_code_line:
-                message_body += f"  • تغيير رقم مقرر {self.employee_id.name} من {original_code_line} إلى {self.code_line}<br/>"
-
-            if self.ancien_index != original_ancien_index:
-                message_body += f"  • تغيير الرقم الإستدلالي لطلبة {self.employee_id.name} من {original_ancien_index} إلى {self.ancien_index}<br/>"
-
-            self.promotion_id.message_post(body=message_body)
+    # @api.multi
+    # def unlink(self):
+    #     for record in self:
+    #         message_body = f"قد تم حذف الموظف {record.employee_id.name} من قائمة الموظفين"
+    #         if message_body:
+    #             record.promotion_id.message_post(body=message_body)
+    #
+    #     return super(RHPromotionLine, self).unlink()
+    #
+    # def write(self, vals):
+    #     original_code_line = self.code_line
+    #     original_ancien_index = self.ancien_index
+    #
+    #     result = super(RHPromotionLine, self).write(vals)
+    #
+    #     if any(field in vals for field in
+    #            ['code_line', 'ancien_index']):
+    #         self._track_changes(original_code_line, original_ancien_index)
+    #
+    #     return result
+    #
+    # def _track_changes(self, original_code_line, original_ancien_index):
+    #     if self.promotion_id:
+    #         message_body = 'تحديث قائمة الموظفين:<br/>'
+    #         if self.code_line != original_code_line:
+    #             message_body += f"  • تغيير رقم مقرر {self.employee_id.name} من {original_code_line} إلى {self.code_line}<br/>"
+    #
+    #         if self.ancien_index != original_ancien_index:
+    #             message_body += f"  • تغيير الرقم الإستدلالي لطلبة {self.employee_id.name} من {original_ancien_index} إلى {self.ancien_index}<br/>"
+    #
+    #         self.promotion_id.message_post(body=message_body)
 
