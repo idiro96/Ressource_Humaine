14153eb14ff7c08d4726b48ccefe28099bd5c77a
lotfi-cetic <lotfi@cetic.dz>
2024-06-24 15:51:21 +0100

    lotfi-cetic : 24/06/2024 - 15:51

--- models/fiche_evaluation.py
+++ models/fiche_evaluation.py
@@ -8,23 +8,25 @@ from datetime import datetime, date
 
 class RHFicheEvaluation(models.Model):
     _name = 'rh.fiche.evaluation'
-    _order = "employee_id"
+    _rec_name = 'exercice'
+    _order = "exercice"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _mail_post_access = 'read'
 
     date_evaluation = fields.Date(required=True, store=True, track_visibility='onchange')
-    employee_id = fields.Many2one('hr.employee', required=True, store=True, track_visibility='onchange')
-    grade_id = fields.Many2one('rh.grade', compute='_onchange_employee_id', store=True)
-    job_id = fields.Many2one('hr.job', compute='_onchange_employee_id', store=True)
-    echelon_id = fields.Many2one('rh.echelon', compute='_onchange_employee_id', store=True)
-    date_grade = fields.Date()
-    note = fields.Integer(default='20', track_visibility='onchange')
-    observation = fields.Char(track_visibility='onchange')
+    # employee_id = fields.Many2one('hr.employee', required=True, store=True, track_visibility='onchange')
+    # grade_id = fields.Many2one('rh.grade', compute='_onchange_employee_id', store=True)
+    # job_id = fields.Many2one('hr.job', compute='_onchange_employee_id', store=True)
+    # echelon_id = fields.Many2one('rh.echelon', compute='_onchange_employee_id', store=True)
+    # date_grade = fields.Date()
+    # note = fields.Integer(default='20', track_visibility='onchange')
+    # observation = fields.Char(track_visibility='onchange')
     fiche_evaluation_file = fields.Binary(track_visibility='onchange')
     exercice = fields.Integer(compute='_compute_exercice', store=True)
     # annee = exercice
     create_uid = fields.Many2one('res.users', string='Created by', readonly=True, track_visibility='onchange')
     write_uid = fields.Many2one('res.users', string='Last Updated by', readonly=True, track_visibility='onchange')
+    fiche_evaluation_lines = fields.One2many('rh.fiche.evaluation.line', 'fiche_evaluation_id')
 
     @api.model
     def create(self, vals):
@@ -38,8 +40,7 @@ class RHFicheEvaluation(models.Model):
 
     @api.multi
     def unlink(self):
-        raise UserError(
-            "لا يمكنك حذف هذا التسجيل")
+        raise UserError("لا يمكنك حذف هذا التسجيل")
         return super(RHFicheEvaluation, self).unlink()
 
     @api.model
@@ -49,22 +50,21 @@ class RHFicheEvaluation(models.Model):
             date_str = vals['date_evaluation']
             exercice = datetime.strptime(date_str, '%Y-%m-%d').year
             # verifier l'existance de l'enregistrement
-            evaluation = self.env['rh.fiche.evaluation'].search(
-                [('employee_id', '=', vals['employee_id']), ('exercice', '=', exercice)])
+            evaluation = self.env['rh.fiche.evaluation'].search([('exercice', '=', exercice)])
             if evaluation:
-                raise UserError("L employee choisit possède déja une notation pour cet exercice")
+                raise UserError("يوجد إستمارة تنقيط بالفعل لهذا التمرين!")
             else:
                 evalutation = super(RHFicheEvaluation, self).create(vals)
                 return evalutation
         else:
-            raise UserError("Entrer la date d'evaluation")
+            raise UserError("Entrer la date d'evaluation !")
 
-    @api.depends('employee_id')
-    def _onchange_employee_id(self):
-        for rec in self:
-            rec.grade_id = rec.employee_id.grade_id
-            rec.job_id = rec.employee_id.job_id
-            rec.echelon_id = rec.employee_id.echelon_id
+    # @api.depends('employee_id')
+    # def _onchange_employee_id(self):
+    #     for rec in self:
+    #         rec.grade_id = rec.employee_id.grade_id
+    #         rec.job_id = rec.employee_id.job_id
+    #         rec.echelon_id = rec.employee_id.echelon_id
 
     @api.depends('date_evaluation')
     def _compute_exercice(self):
@@ -73,8 +73,50 @@ class RHFicheEvaluation(models.Model):
             if rec.date_evaluation:
                 date_str = rec.date_evaluation
                 rec.exercice = datetime.strptime(date_str, '%Y-%m-%d').year
-                evaluation = self.env['rh.fiche.evaluation'].search(
-                    [('employee_id', '=', rec.employee_id.id), ('exercice', '=', rec.exercice)])
+                evaluation = self.env['rh.fiche.evaluation'].search([('exercice', '=', rec.exercice)])
                 if evaluation:
-                    raise UserError("L employee choisit possède déja une notation pour cet exercice")
+                    raise UserError("يوجد إستمارة تنقيط بالفعل لهذا التمرين!")
 
+    @api.model
+    def create(self, vals):
+        evaluation = super(RHFicheEvaluation, self).create(vals)
+        employee = self.env['hr.employee'].search(
+            [('code_type_fonction', '!=', 'contractuel'), ('fin_relation', '=', False)], order='name')
+        for rec in employee:
+            fiche_evaluation_line = self.env['rh.fiche.evaluation.line'].create({
+                'employee_id': rec.id,
+                'fiche_evaluation_id': evaluation.id,
+            })
+        return evaluation
+
+
+class RHFicheEvaluationLine(models.Model):
+    _name = 'rh.fiche.evaluation.line'
+    _rec_name = 'employee_id'
+
+    fiche_evaluation_id = fields.Many2one('rh.fiche.evaluation', tracking=True)
+    employee_id = fields.Many2one('hr.employee')
+    duree = fields.Selection(selection=[('inferieure', 'Inferieure'), ('moyen', 'Moyen'), ('superieure', 'Supérieure')],
+                             default='inferieure')
+    note = fields.Integer(default='20', track_visibility='onchange')
+    observation = fields.Char(track_visibility='onchange')
+    create_uid = fields.Many2one('res.users', string='Created by', readonly=True, track_visibility='always')
+    write_uid = fields.Many2one('res.users', string='Last Updated by', readonly=True, track_visibility='always')
+
+    @api.constrains('note')
+    def chek_notation(self):
+        for rec in self:
+            if rec.note > 20:
+                raise UserError("يجب أن يكون التنقيط بين 0 و 20!")
+
+    @api.constrains('employee_id')
+    def _check_duplicate_employee(self):
+        for rec in self:
+            existing_employees = self.search([
+                ('fiche_evaluation_id', '=', rec.fiche_evaluation_id.id),
+                ('employee_id', '=', rec.employee_id.id),
+                ('id', '!=', rec.id)
+            ])
+            if existing_employees:
+                employee_name = rec.employee_id.name
+                raise ValidationError(f'لا يمكنك إضافة الموظف {employee_name} مرتين!')
--- reports/fiche_evaluation.py
+++ reports/fiche_evaluation.py
@@ -9,39 +9,47 @@ class FicheEvaluation(models.AbstractModel):
 
     @api.model
     def get_report_values(self, docids, data=None):
-        employee_evaluee = self.env['rh.fiche.evaluation'].browse(docids[0])
-        employee_info = self.env['hr.employee'].search([('id', '=', employee_evaluee.employee_id.id)])
-        # print(employee_evaluee.grade_id.intitule_grade)
-
-        date_new_avancement = fields.Date.from_string(employee_evaluee.date_evaluation) if employee_evaluee.date_evaluation else None
-        date_old_avancement = fields.Date.from_string(employee_info.date_grade) if employee_info.date_grade else None
-
-        years, months, days = 0, 0, 0
-
-        if date_new_avancement and date_old_avancement:
-            delta = relativedelta(date_new_avancement, date_old_avancement)
-            years = delta.years
-            months = delta.months
-            days = delta.days
-
-        time_difference = f"قدرها {years} سنة و {months} شهر و {days} يوم"
-
-        formatted_date_grade = None
-        formatted_birthday = None
-
-        if employee_info.date_grade:
-            date_grade = employee_info.date_grade
-            formatted_date_grade = datetime.strptime(date_grade, "%Y-%m-%d").strftime("%Y/%m/%d")
-
-        if employee_info.birthday:
-            birthday = employee_info.birthday
-            formatted_birthday = datetime.strptime(birthday, "%Y-%m-%d").strftime("%Y/%m/%d")
-
+        evaluation = self.env['rh.fiche.evaluation'].browse(docids[0])
+        fiche_evaluation_lines = self.env['rh.fiche.evaluation.line'].search([('fiche_evaluation_id', '=', evaluation.id)])
+        lines_data = []
+        for rec in fiche_evaluation_lines:
+            date_new_avancement = fields.Date.from_string(rec.fiche_evaluation_id.date_evaluation) if rec.fiche_evaluation_id.date_evaluation else None
+            date_old_avancement = fields.Date.from_string(rec.employee_id.date_grade) if rec.employee_id.date_grade else None
+            years, months, days = 0, 0, 0
+            if date_new_avancement and date_old_avancement:
+                delta = relativedelta(date_new_avancement, date_old_avancement)
+                years = delta.years
+                months = delta.months
+                days = delta.days
+            time_difference = f"قدرها {years} سنة و {months} شهر و {days} يوم"
+            formatted_date_grade = None
+            formatted_birthday = None
+            if rec.employee_id.date_grade:
+                date_grade = rec.employee_id.date_grade
+                formatted_date_grade = datetime.strptime(date_grade, "%Y-%m-%d").strftime("%Y/%m/%d")
+            if rec.employee_id.birthday:
+                birthday = rec.employee_id.birthday
+                formatted_birthday = datetime.strptime(birthday, "%Y-%m-%d").strftime("%Y/%m/%d")
+            line_data = {
+                'nom': rec.employee_id.nom_ar,
+                'prenom': rec.employee_id.prenom_ar,
+                'grade': rec.employee_id.grade_id.intitule_grade,
+                'description': rec.employee_id.grade_id.description,
+                'job': rec.employee_id.job_id.name,
+                'echelon': rec.employee_id.echelon_id.intitule,
+                'note': rec.note,
+                'observation': rec.observation,
+                'time_difference': time_difference,
+                'duree': rec.duree,
+                'status': rec.employee_id.marital,
+                'gender': rec.employee_id.gender,
+                'date_grade': formatted_date_grade,
+                'birthday': formatted_birthday,
+            }
+            lines_data.append(line_data)
+        print(lines_data)
         report_data = {
-            'employee_evaluee': employee_evaluee,
-            'employee_info': employee_info,
-            'duree': time_difference,
-            'date_grade': formatted_date_grade,
-            'birthday': formatted_birthday,
+            'evaluation': evaluation,
+            'fiche_evaluation_lines': lines_data,
         }
         return report_data
