777cf9841c707d96d7bef8df19875c1f0bb94911
benzinebasmaa <BENYOUCEF.Rabah@cetic.dz>
2024-11-18 09:40:49 +0100

    pull

--- reports/fiche_information.py
+++ reports/fiche_information.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from odoo import models, fields, api, _
 
 
@@ -7,14 +9,19 @@ class FicheInformationReport(models.AbstractModel):
 
     @api.model
     def get_report_values(self, docids, data=None):
-
-        employees= self.env['hr.employee'].browse(docids)
-
-
-
+        employees = self.env['hr.employee'].browse(docids)
+        birthday_employee = {}
+        for rec in employees:
+            date_birthday_str = rec.birthday
+            if date_birthday_str:
+                formatted_date = datetime.strptime(date_birthday_str, "%Y-%m-%d").strftime("%d-%m-%Y")
+                birthday_employee[rec.id] = formatted_date
+            else:
+                birthday_employee[rec.id] = ''
         report_data = {
             'company': self.env.user.company_id,
             "employee": employees,
+            'birthday_employee': birthday_employee,
 
 
         }
